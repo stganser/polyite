0f08572c761564d756703b7c7746f7341668f0be
Stefan Ganser <ganser@fim.uni-passau.de>
2019-02-11 19:12:15 +0100

    More tests

--- test/polyite/fitness/TestMemAccPattern.scala
+++ test/polyite/fitness/TestMemAccPattern.scala
@@ -22,15 +22,15 @@ class TestMemAccPattern {
       .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> A[i, j] }"))
     val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
     val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
-    val fVal :Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
     println("all: " + fVal)
     assertEquals(1.0, fVal, 0)
     val sched1 : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i, j] -> [j, i] }"), domInfo, scop, deps, false, true), deps))
-    val fVal1 :Double = MemAccessPattern.calc(sched1, null, scop, null, domInfo, deps)
+    val fVal1 : Double = MemAccessPattern.calc(sched1, null, scop, null, domInfo, deps)
     println("all interchanged: " + fVal1)
     assertEquals(0.0, fVal1, 0)
   }
-  
+
   @Test
   def testSome() {
     val scop : ScopInfo = new ScopInfo().setParams(isl.Set.readFromStr(Isl.ctx, "[n] -> { : 0 < n }"))
@@ -40,15 +40,15 @@ class TestMemAccPattern {
       .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> A[i, j] }"))
     val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
     val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
-    val fVal :Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
     println("some: " + fVal)
     assertEquals(0.5, fVal, 0)
     val sched1 : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i, j] -> [j, i] }"), domInfo, scop, deps, false, true), deps))
-    val fVal1 :Double = MemAccessPattern.calc(sched1, null, scop, null, domInfo, deps)
+    val fVal1 : Double = MemAccessPattern.calc(sched1, null, scop, null, domInfo, deps)
     println("some interchanged: " + fVal1)
     assertEquals(0.5, fVal1, 0)
   }
-  
+
   @Test
   def testNone() {
     val scop : ScopInfo = new ScopInfo().setParams(isl.Set.readFromStr(Isl.ctx, "[n] -> { : 0 < n }"))
@@ -58,15 +58,15 @@ class TestMemAccPattern {
       .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> A[j, i] }"))
     val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
     val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
-    val fVal :Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
     println("none: " + fVal)
     assertEquals(0.0, fVal, 0)
     val sched1 : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i, j] -> [j, i] }"), domInfo, scop, deps, false, true), deps))
-    val fVal1 :Double = MemAccessPattern.calc(sched1, null, scop, null, domInfo, deps)
+    val fVal1 : Double = MemAccessPattern.calc(sched1, null, scop, null, domInfo, deps)
     println("none interchanged: " + fVal1)
     assertEquals(1.0, fVal1, 0)
   }
-  
+
   @Test
   def testSkewedSome() {
     val scop : ScopInfo = new ScopInfo().setParams(isl.Set.readFromStr(Isl.ctx, "[n] -> { : 0 < n }"))
@@ -76,11 +76,11 @@ class TestMemAccPattern {
       .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> A[i, j + n] }"))
     val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
     val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
-    val fVal :Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
     println("skewed some 1: " + fVal)
     assertEquals(0.5, fVal, 0)
   }
-  
+
   @Test
   def testSkewedSome1() {
     val scop : ScopInfo = new ScopInfo().setParams(isl.Set.readFromStr(Isl.ctx, "[n] -> { : 0 < n }"))
@@ -90,11 +90,11 @@ class TestMemAccPattern {
       .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> A[i, j] }"))
     val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
     val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
-    val fVal :Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
     println("skewed some 2: " + fVal)
     assertEquals(0.5, fVal, 0)
   }
-  
+
   @Test
   def testSkewedNone() {
     val scop : ScopInfo = new ScopInfo().setParams(isl.Set.readFromStr(Isl.ctx, "[n] -> { : 0 < n }"))
@@ -104,8 +104,50 @@ class TestMemAccPattern {
       .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> A[i + j, j] }"))
     val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
     val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
-    val fVal :Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
     println("skewed none: " + fVal)
     assertEquals(0.0, fVal, 0)
+  }
+
+  def testWithSeqNodes() {
+    val scop : ScopInfo = new ScopInfo().setParams(isl.Set.readFromStr(Isl.ctx, "[n] -> { : 0 < n }"))
+      .addDomain(isl.Set.readFromStr(Isl.ctx, "[n] -> { R[i, j] : 0 <=i <= n and 0 <= j <= n }"))
+      .addSchedule(isl.Map.readFromStr(Isl.ctx, "[n] -> { R[i, j] -> [i, 1, j] }"))
+      .addDomain(isl.Set.readFromStr(Isl.ctx, "[n] -> { S[i, j] : 0 <=i <= n and 0 <= j <= n }"))
+      .addSchedule(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i, j] -> [i, 2, j] }"))
+      .addDomain(isl.Set.readFromStr(Isl.ctx, "[n] -> { T[i, j] : 0 <=i <= n and 0 <= j <= n }"))
+      .addSchedule(isl.Map.readFromStr(Isl.ctx, "[n] -> { T[i, j] -> [i, 3, j] }"))
+      .addWrs(isl.Map.readFromStr(Isl.ctx, "[n] -> { R[i,j] -> A[i, j] }"))
+      .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { R[i,j] -> A[i + j, j] }"))
+      .addWrs(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> B[j, i + j] }"))
+      .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> B[i, j] }"))
+      .addWrs(isl.Map.readFromStr(Isl.ctx, "[n] -> { T[i,j] -> C[j, i] }"))
+      .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { T[i,j] -> C[i, i] }"))
+    val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
+    val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    println("with seq: " + fVal)
+    assertEquals(0.5, fVal, 0)
+  }
+
+  def testWithSeqNodes1() {
+    val scop : ScopInfo = new ScopInfo().setParams(isl.Set.readFromStr(Isl.ctx, "[n] -> { : 0 < n }"))
+      .addDomain(isl.Set.readFromStr(Isl.ctx, "[n] -> { R[i, j] : 0 <=i <= n and 0 <= j <= n }"))
+      .addSchedule(isl.Map.readFromStr(Isl.ctx, "[n] -> { R[i, j] -> [i, 1, j] }"))
+      .addDomain(isl.Set.readFromStr(Isl.ctx, "[n] -> { S[i, j] : 0 <=i <= n and 0 <= j <= n }"))
+      .addSchedule(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i, j] -> [i, 2, j] }"))
+      .addDomain(isl.Set.readFromStr(Isl.ctx, "[n] -> { T[i, j] : 0 <=i <= n and 0 <= j <= n }"))
+      .addSchedule(isl.Map.readFromStr(Isl.ctx, "[n] -> { T[i, j] -> [i, 3, j] }"))
+      .addWrs(isl.Map.readFromStr(Isl.ctx, "[n] -> { R[i,j] -> A[i, j] }"))
+      .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { R[i,j] -> A[i + j, j] }"))
+      .addWrs(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> B[j, i + j] }"))
+      .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { S[i,j] -> B[j, i] }"))
+      .addWrs(isl.Map.readFromStr(Isl.ctx, "[n] -> { T[i,j] -> C[j, i] }"))
+      .addRds(isl.Map.readFromStr(Isl.ctx, "[n] -> { T[i,j] -> C[i, i] }"))
+    val (deps : Set[Dependence], domInfo : DomainCoeffInfo) = ScheduleSpaceUtils.calcDepsAndDomInfo(scop)
+    val sched : ScheduleNode = SchedTreeUtil.markLoops(SchedTreeUtil.simplifySchedTree(ScheduleTreeConstruction.islUnionMap2BasicScheduleTree(scop.getSched, domInfo, scop, deps, false, true), deps))
+    val fVal : Double = MemAccessPattern.calc(sched, null, scop, null, domInfo, deps)
+    println("with seq 1: " + fVal)
+    assertEquals(2.0 / 6.0, fVal, 0)
   }
 }
